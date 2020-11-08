3edc2d7b1271059fba2b2c0e5994460f336fd46d
Livia <liviasithmendes@gmail.com>
2020-11-08 17:07:11 -0300

    Creation of Category and Product objects, with their migrations.

--- siteTechStartPro/core/models.py
+++ siteTechStartPro/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
